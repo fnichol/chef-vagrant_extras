73e8cd2915236a836064543c338cc9e8368d74f2
Fletcher Nichol <fnichol@nichol.ca>
2010-12-04 00:43:41 -0700

    Add vagrant to /etc/sudoers.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+node[:authorization][:sudo][:extensions] << <<-SUDO.gsub(/^ {2}/, '')
+### ensure vagrant user has sudo ###
+  vagrant ALL=(ALL) ALL
+SUDO
