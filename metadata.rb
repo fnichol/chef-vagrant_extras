bddb3c3376066ccc861f5e21b631ae5c35da82d7
Fletcher Nichol <fnichol@nichol.ca>
2012-03-05 10:45:40 -0700

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Extra fixes and shims for running your recipes in a Vagrant VM."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 supports "ubuntu"
 supports "debian"
